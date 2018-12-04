0186a7521e0078bfd2a1a528c7442c57e4348810
Yurchenko Denis <den4ik24@gmail.com>
2018-12-04 21:53:10 +0200

    Добавьте файлы проекта.

---
+++ intent/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+using Android.Content;
+
+namespace intent
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText editText;
+        Button button1;
+
+                     
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            editText = FindViewById<EditText>(Resource.Id.editText);
+            button1 = FindViewById<Button>(Resource.Id.button1);
+
+            
+
+            button1.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(NewActivity));
+            intent.PutExtra("InOut", editText.Text);
+            StartActivity(intent);
+        }
+    }
+}
---
+++ intent/NewActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+
+namespace intent
+{
+    [Activity(Label = "NewActivity")]
+    public class NewActivity : AppCompatActivity
+    {
+        TextView text;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.NewActivity);
+
+            text = FindViewById<TextView>(Resource.Id.text);
+
+            var intent = Intent;
+            string result = intent.GetStringExtra("InOut");
+            text.Text = "HELLO " + result;
+        }
+    }
+}
