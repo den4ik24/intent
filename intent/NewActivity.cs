e46fb0db094cbace5736170a6e6bacfbd77d5e7f
Yurchenko Denis <den4ik24@gmail.com>
2018-12-22 21:45:34 +0200

    крайнее

--- intent/NewActivity.cs
+++ intent/NewActivity.cs
@@ -10,7 +10,7 @@ using System;
 
 namespace intent
 {
-    [Activity(Label = "привет Друг")]
+    [Activity(Label = "привет, друг")]
     public class NewActivity : AppCompatActivity
     {
         string dbPath = Path.Combine(System.Environment.GetFolderPath
